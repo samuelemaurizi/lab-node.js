0983de8bd5024369bb2b2550817b3ef2678bdcc1
Samuele Maurizi <samuelemaurizi@gmail.com>
2019-09-05 14:19:34 +0200

    modify delete product in async way

--- controllers/admin.js
+++ controllers/admin.js
@@ -185,8 +185,8 @@ exports.getProducts = (req, res, next) => {
 
 ////////////////////
 // POST DELETE PRODUCT
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -197,12 +197,10 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
-      console.log('PRODUCT DELETED!');
-      res.redirect('/admin/products');
+      console.log('>>> PRODUCT DELETED!');
+      res.status(200).json({ message: 'Success!' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Deleting product failed.' });
     });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -53,6 +53,6 @@ router.post(
 
 ////////////////////
 // POST DELETE
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;
